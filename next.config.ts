e0b1b15a2f4231071f2d1123dd96b7a4f72faf46
KeyHelper Team <keyhelperteam@gmail.com>
2026-03-09 00:07:06 -0400

    testing

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: process.env.GITHUB_PAGES === 'true' ? '/heavenapp' : '',
-  },
 };
 
 export default nextConfig;
